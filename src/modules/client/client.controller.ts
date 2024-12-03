e1376a06a190e446fe249c342fd2f3b5e2db5f73
Klee Collage <klee.collage@gmail.com>
2024-12-03 14:39:55 -0600

    documentation: Add swagger API documentation to controllers

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -7,34 +7,170 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ClientService } from 'src/modules/client/client.service';
 import { ClientDto } from 'src/modules/client/dto/client-dto';
 
 @Controller('api/v1/clients')
+@ApiTags('clients')
 export class ClientController {
   constructor(private clientService: ClientService) {}
 
   @Post()
+  @ApiOperation({
+    description: 'Create a new client',
+  })
+  @ApiBody({
+    description: 'Create a new client using ClientDto',
+    type: ClientDto,
+    examples: {
+      example1: {
+        value: {
+          id: 1,
+          name: 'Michael Johnson',
+          email: 'm.johnson@mail.com',
+          address: {
+            country: 'USA',
+            province: 'FLorida',
+            town: 'Miami',
+            street: 'punta azul st 32',
+          },
+        },
+      },
+      example2: {
+        value: {
+          name: 'John Doe',
+          email: 'j.doe@mail.com',
+          address: {
+            country: 'Mexico',
+            province: 'Guadalajara',
+            town: 'Jalisco',
+            street: 'tequila no 123',
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Client created successfully',
+  })
+  @ApiResponse({
+    status: 409,
+    description: /*html*/ `Client already exists<br/>
+                          Address is already registered
+                          `,
+  })
   createClient(@Body() client: ClientDto) {
     return this.clientService.createClient(client);
   }
 
   @Get()
+  @ApiOperation({
+    description: 'Return all clients',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data returned successfully',
+  })
   getClients() {
     return this.clientService.getClients();
   }
 
   @Get('/:id')
+  @ApiOperation({
+    description: 'Given its id, return one client and his address',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    required: true,
+    description: 'Client ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data returned successfully',
+  })
   getClientById(@Param('id') id: number) {
     return this.clientService.getClientById(id);
   }
 
   @Put()
+  @ApiOperation({
+    description: 'Update one client',
+  })
+  @ApiBody({
+    description: 'Update one client using ClientDto',
+    type: ClientDto,
+    examples: {
+      example1: {
+        value: {
+          id: 1,
+          name: 'Jane Smith',
+          email: 'j.smith@mail.com',
+          address: {
+            id: 1,
+            country: 'USA',
+            province: 'California',
+            town: 'Las Vegas',
+            street: 'rodeo st. 32',
+          },
+        },
+      },
+      example2: {
+        value: {
+          id: 2,
+          name: 'James Brown',
+          email: 'j.brown@mail.com',
+          address: {
+            id: 2,
+            country: 'USA',
+            province: 'Florida',
+            town: 'Miami',
+            street: 'cabo verde st. 32',
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Client updated successfully',
+  })
+  @ApiResponse({
+    status: 409,
+    description: /*html*/ `Client already exists<br/>
+                          Address is already registered
+                          `,
+  })
   updateClient(@Body() client: ClientDto) {
     return this.clientService.updateClient(client);
   }
 
   @Delete('/:id')
+  @ApiOperation({
+    description: 'Givens its id, delete a client and his address',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    required: true,
+    description: 'Client ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Client and his address deleted successfully',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Client not exists',
+  })
   deleteClient(@Param('id') id: number) {
     return this.clientService.deleteClient(id);
   }
--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -7,25 +7,94 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { OrderDto } from 'src/modules/order/dto/order-dto';
 import { OrderService } from 'src/modules/order/order.service';
 import { ParseDatePipe } from 'src/pipes/parse-date.pipe';
 
 @Controller('api/v1/orders')
+@ApiTags('orders')
 export class OrderController {
   constructor(private orderService: OrderService) {}
 
   @Post()
+  @ApiOperation({
+    description: 'Create order',
+  })
+  @ApiBody({
+    description: 'Create an order using OrderDto',
+    type: OrderDto,
+    examples: {
+      example1: {
+        value: {
+          client: { id: 1 },
+          products: [{ id: 3 }, { id: 2 }],
+        },
+      },
+      example2: {
+        value: {
+          confirmAt: '2024-01-30',
+          client: { id: 2 },
+          products: [{ id: 1 }, { id: 3 }],
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Order created successfully',
+  })
+  @ApiResponse({
+    status: 409,
+    description: /*html*/ `Client not exists<br />
+                          Product not exists<br />
+                          Product status is deleted
+                          `,
+  })
   createOrder(@Body() order: OrderDto) {
     return this.orderService.createOrder(order);
   }
 
   @Get('/pending')
+  @ApiOperation({
+    description: 'Returns all pending orders or without confirm',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data returned correctly',
+  })
   getPendingOrders() {
     return this.orderService.getPendingOrders();
   }
 
   @Get('/confirmed')
+  @ApiOperation({
+    description:
+      'Returns all confirmed orders, with the option to filter between start and end dates',
+  })
+  @ApiQuery({
+    name: 'start',
+    type: Date,
+    required: false,
+    description: 'Filter orders from the confirmation date',
+  })
+  @ApiQuery({
+    name: 'end',
+    type: Date,
+    required: false,
+    description: 'Filter orders from the confirmation date',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data returned correctly',
+  })
   getConfirmedOrders(
     @Query('start', ParseDatePipe) start: Date,
     @Query('end', ParseDatePipe) end: Date,
@@ -34,16 +103,61 @@ export class OrderController {
   }
 
   @Get('/:id')
+  @ApiOperation({
+    description: 'Givens its id, return all information order',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    required: true,
+    description: 'Order ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data returned succesfully',
+  })
   getOrderById(@Param('id') id: string) {
     return this.orderService.getOrderById(id);
   }
 
   @Get('/client/:id')
-  getOrdersByClient(@Param('id') id: number) {
-    return this.orderService.getOrdersByClient(id);
+  @ApiOperation({
+    description: 'Return all customer orders',
+  })
+  @ApiParam({
+    name: 'idClient',
+    type: Number,
+    required: true,
+    description: 'Client ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data returned succesfully',
+  })
+  getOrdersByClient(@Param('idClient') idClient: number) {
+    return this.orderService.getOrdersByClient(idClient);
   }
 
   @Patch('/confirm/:id')
+  @ApiOperation({
+    description: 'Sets a date for the confirmed field',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    required: true,
+    description: 'Order ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'New date seted on confirmAt succesfully',
+  })
+  @ApiResponse({
+    status: 409,
+    description: /*html*/ `Confirm is already stablish<br />
+                            Order not exists
+                          `,
+  })
   confirmOrder(@Param('id') id: string) {
     return this.orderService.confirmOrder(id);
   }
